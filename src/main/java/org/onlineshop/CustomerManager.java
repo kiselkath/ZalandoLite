8427ee51d6a5a9bcec20a4b37bc131c2bd7b0304
katekiselevich <kiselkath@gmail.com>
2025-06-02 14:45:17 +0200

    Updated CustomerManager.java

--- src/main/java/org/onlineshop/CustomerManager.java
+++ src/main/java/org/onlineshop/CustomerManager.java
@@ -1,4 +1,25 @@
 package org.onlineshop;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Что это: Управляющий клиентами: регистрация, поиск, хранение.
+ * Зачем нужен: Позволяет быстро находить клиента по ID. Это нужно при создании заказа, отправке email и т.д.
+ */
+
 public class CustomerManager {
+
+    // Карта клиентов по ID
+    private static Map<Integer, Customer> customerMap = new HashMap<>();
+
+    public static void registerCustomer(Customer c) {
+        if (c == null) return;
+        customerMap.put(c.getId(), c);
+    }
+
+    public static Customer getCustomerById(int id) {
+        return customerMap.get(id);
+    }
+
 }
