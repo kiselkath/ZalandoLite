78cd6c288ddfac8059851c99b19a7114e9be8393
katekiselevich <kiselkath@gmail.com>
2025-06-02 14:29:59 +0200

    Updated class Customer.java

--- src/main/java/org/onlineshop/Customer.java
+++ src/main/java/org/onlineshop/Customer.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class Customer {
     private int id;
-    public String name;
-    public String email;
-    public List<String> favoriteCategories;
-    public boolean isVip;
+    private String name;
+    private String email;
+    private List<String> favoriteCategories;
+    private boolean isVip;
 
     public Customer(int id, String name, String email, List<String> favoriteCategories, boolean isVip) {
         this.id = id;
@@ -19,5 +19,39 @@ public class Customer {
 
     public int getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<String> getFavoriteCategories() {
+        return favoriteCategories;
+    }
+
+    public boolean isVip() {
+        return isVip;
+    }
+
+    public void setFavoriteCategories(List<String> favoriteCategories) {
+        this.favoriteCategories = favoriteCategories;
+    }
+
+    public void setVip(boolean vip) {
+        isVip = vip;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer: " +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                ", email = '" + email + '\'' +
+                ", favoriteCategories = " + favoriteCategories +
+                ", isVip = " + isVip;
     }
 }
